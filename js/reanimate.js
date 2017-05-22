67032caee01435e8776f5f43ee854431e6d82dd9
JChavannes01 <janko.chavannes@gmail.com>
2017-05-22 15:55:42 +0200

    Edited naming.

--- js/reanimate.js
+++ js/reanimate.js
@@ -1,11 +1,11 @@
 window.onload=function(){
-		var x = document.getElementsByClassName("Janko-restart");
+		var x = document.getElementsByClassName("animate-restart");
 		var i;
 		for (i = 0; i < x.length; i++) {
 		  x[i].addEventListener("webkitAnimationEnd", myEndFunction);
 		}
 		document.getElementById("clickMe").onclick = function() {
-		  var x = document.getElementsByClassName("Janko-restart");
+		  var x = document.getElementsByClassName("animate-restart");
 		  var i;
 		  for (i = 0; i < x.length; i++) {
 			x[i].style.WebkitAnimation = "";
